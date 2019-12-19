30f14d4835e5aad5b46ac71943631a8cbb25ee62
Renzo Carara <renzo.carara@libero.it>
2019-12-19 12:35:58 +0100

    sistemato commenti e codice. variabili con valori "costanti" rese globali spostatandole al di fuori delle functions

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // Nome repo: ajax-ex-boolflix
 // -----------------------------------------------------------------------------
 
-// ---------------------------- CONSTANTs --------------------------------------
-// variabili globali che non cambiano valore durante l'esecuzione dello script (costanti)
+// ------------------------------- CONSTANTs -----------------------------------
+var APIurl = 'https://api.themoviedb.org/3'; // indirizzo base API TMDB
+var APIendpointMovie = '/search/movie'; // endpoint dell'API
+var APIendpointTV = '/search/tv'; // endpoint dell'API
+var APIkey = '541a69e2ef5cfc0e4d5d4e563ef1de78'; // la mia chiave per le API TDMB
+var lang = 'it-IT'; // parametro lingua, quando costruisco la richiesta all'API
+
+var imgUrlFixed = 'https://image.tmdb.org/t/p/'; // indirizzo base per le immagini
+var imgUrlSize = 'w342/'; // dimensione dell'immagine
+var imgNotAvailable = "images/no_poster.png"; // immagine di default
+
 // array con le lingue per cui è disponibile una bandierina da visualizare
-availableFlags = ['bg', 'zh', 'cs', 'de', 'dk', 'en', 'es', 'et', 'fi', 'fr', 'gr', 'hr', 'hu', 'in', 'is',
+var availableFlags = ['bg', 'zh', 'cs', 'de', 'dk', 'en', 'es', 'et', 'fi', 'fr', 'gr', 'hr', 'hu', 'in', 'is',
     'it', 'ja', 'lv', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'sl', 'sv', 'tr', 'ua'
 ];
 
 $(document).ready(function() {
-
     //intercetto click su bottone per la ricerca
     $('#search-button').click(function() {
         var searchInput = $('#search-input').val(); // recupero la stringa da ricercare
@@ -56,12 +64,6 @@ function handleUserSearch(searchString) {
     // verifica se c'è una stringa da ricercare e nel caso fa una chiamata AJAX
     // dopodichè resetta il campo di ricerca e svuota il contenitore delle cards
 
-    var APIurl = 'https://api.themoviedb.org/3';
-    var APIendpointMovie = '/search/movie';
-    var APIendpointTV = '/search/tv';
-    var APIkey = '541a69e2ef5cfc0e4d5d4e563ef1de78';
-    var lang = 'it-IT';
-
     // verifico che la stringa non sia nulla, se la stringa è nulla non faccio niente
     if (searchString) {
         // chiamata AJAX per recuperare i dati ricercati tramite API -- CERCO I MOVIES
@@ -106,16 +108,12 @@ function handleResponse(data, endpoint) {
     // crea un oggetto per HANDLEBARS per valorizzare il template
     // chiama diverse altre funzioni che preparano i singoli valori delle proprietà dell'oggetto
 
-    var APIendpointMovie = '/search/movie'; // endpoint dell'API
-    var APIendpointTV = '/search/tv'; // endpoint dell'API
-
     if (data.total_results > 0) { // ci sono dei risultati da elaborare
 
         var title; // titolo del film o della serie TV
         var original_title; // titolo originale del film o della serie TV
         var results = data.results; // estraggo la parte di risultati che mi interessa
         var MovieOrTV; // distingue se FILM o SERIE TV
-
 
         // recupero il codice html dal template HANDLEBARS
         var cardTemplate = $('#card-template').html();
@@ -154,8 +152,10 @@ function handleResponse(data, endpoint) {
 
             // aggiungo nella mia pagina le cards, ovvero il codice HTML generato da HANDLEBARS
             if (MovieOrTV == "Film") {
+                // li aggiungo nella sezione Film
                 $('#movies-container').append(card);
             } else {
+                // li aggiungo nella sezione Serie TV
                 $('#series-container').append(card);
             }
 
@@ -180,11 +180,7 @@ function createImgLink(posterLink) {
     // crea il path completo per recuperare l'immagine (poster) del film/serie tv
     // gestisce il caso in cui il poster non è disponibile
 
-    // var defaultPoster = 'images/no_preview_poster.png';
-    var imgUrlFixed = 'https://image.tmdb.org/t/p/'; // indirizzo base per le immagini
-    var imgUrlSize = 'w342/'; // dimensione dell'immagine
     var imgUrlVariable = posterLink; // indirizzo specifico dell'immagine richiesta tramite API
-    var imgNotAvailable = "images/no_poster.png";
     var path = "";
 
     if (imgUrlVariable != null) {
@@ -212,7 +208,7 @@ function createFlag(lang) {
         flagOrText = '<img class="flag" src="images/' + lang + '.svg" ' + 'alt="ISO 639-1: ' + lang + '">';
     } else {
         // la bandierina per quel linguaggio non è disponibile, restituisco solo testo (il codice lingua)
-        flagOrText = lang;
+        flagOrText = "codice ISO-693-1: " + lang;
     }
 
     return flagOrText;
@@ -256,55 +252,3 @@ function createOverview(text) {
     }
     return textToBeDisplayed;
 }
-
-
-
-// mil5
-// chiamata con get/movie{move_id} ilmovie_id lo leggo dall'oggetto della chiamata search/movie
-// https://api.themoviedb.org/3/movie/25606?api_key=541a69e2ef5cfc0e4d5d4e563ef1de78&language=it-IT
-var oggetto = {
-    "adult": false,
-    "backdrop_path": "/17UicwOSvWjHUpT5arG20PoM66e.jpg",
-    "belongs_to_collection": {
-        "id": 41658,
-        "name": "Fantozzi - Collezione",
-        "poster_path": "/f2nxUik9UU3AOquFGCzKBzcxeY0.jpg",
-        "backdrop_path": "/h2L6fnTYieCrNscxbtjGG31lBRm.jpg"
-    },
-    "budget": 0,
-    "genres": [{
-        "id": 35,
-        "name": "Commedia"
-    }],
-    "homepage": "",
-    "id": 25606,
-    "imdb_id": "tt0071486",
-    "original_language": "it",
-    "original_title": "Fantozzi",
-    "overview": "Dopo aver lottato con i mezzi pubblici perennemente strapieni, il ragionier Ugo Fantozzi arriva finalmente in ufficio. Qui trova ad attenderlo pile di pratiche da sbrigare rifilategli dai furbi colleghi. Costretto a partecipare a gite aziendali e partite di calcio, Fantozzi è anche ossessionato da una moglie brutta e da una figlia orripilante.",
-    "popularity": 6.258,
-    "poster_path": "/oHqHe0MNqcbmw5Qa3075KIzE4G7.jpg",
-    "production_companies": [{
-        "id": 12778,
-        "logo_path": null,
-        "name": "Rizzoli Film",
-        "origin_country": ""
-    }],
-    "production_countries": [{
-        "iso_3166_1": "IT",
-        "name": "Italy"
-    }],
-    "release_date": "1975-03-27",
-    "revenue": 0,
-    "runtime": 103,
-    "spoken_languages": [{
-        "iso_639_1": "it",
-        "name": "Italiano"
-    }],
-    "status": "Released",
-    "tagline": "",
-    "title": "Fantozzi",
-    "video": false,
-    "vote_average": 7.9,
-    "vote_count": 461
-};
